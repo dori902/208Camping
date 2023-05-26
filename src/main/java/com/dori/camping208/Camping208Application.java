01c06fab67e91a0bb10140652c617b534a68a2f0
Dori <kigndori902@naver.com>
2023-05-27 03:19:34 +0900

    디폴트 작업

---
+++ src/main/java/com/dori/camping208/Camping208Application.java
@@ -0,0 +1,13 @@
+package com.dori.camping208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camping208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Camping208Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dori/camping208/Camping208ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dori.camping208;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Camping208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
